52d0c56ecda856aa415d615df76229a8b7909ae9
Dayal Sudhakar <Dayalpython@gmail.com>
2024-02-12 22:57:03 +0530

    navbar responsvie

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import Home from "./Pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 const App = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -10,28 +15,47 @@ const App = () => {
   };
   return (
     <div className="bg-[#121212] text-white flex justify-center">
-      <div className="w-[90%]">
+      <div className="w-[84%] lg:w-[85%] xl:w-4/5 2xl:w-[75%]">
         <BrowserRouter>
           <Navbar handleNavbar={handleNavbar} showNavbar={showNavbar} />
           <div className="flex justify-center md:hidden">
             {showNavbar && (
               <div className="w-[100%] bg-[#121212] h-52 left-0 flex justify-center absolute">
-                <ul className="space-y-2 ">
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>Services</li>
-                  <li>Contact</li>
-                  <li>Services</li>
-                  <li>Login</li>
+                <ul className="">
+                  <Link to="/">
+                    <li className="py-1">Home</li>
+                  </Link>
+                  <Link to="/about">
+                    <li className="py-1">About</li>
+                  </Link>
+                  <Link to="/services">
+                    <li className="py-1">Services</li>
+                  </Link>
+                  <Link to="/contact">
+                    <li className="py-1">Contact</li>
+                  </Link>
+                  <Link to="/register">
+                    <li className="py-1">Register</li>
+                  </Link>
+                  <Link to="/login">
+                    <li className="py-1">Login</li>
+                  </Link>
                 </ul>
               </div>
             )}
           </div>
-          <Routes>
-            <Route>
-              <Route path="/" element={<Home />} />
-            </Route>
-          </Routes>
+          <div onClick={handleNavbar}>
+            <Routes>
+              <Route>
+                <Route path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+              </Route>
+            </Routes>
+          </div>
         </BrowserRouter>
       </div>
     </div>
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,26 +1,41 @@
 import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ showNavbar, handleNavbar }) => {
   return (
-    <div className="text-[#5358EB]  relative  py-2">
+    <div className="text-[#5358EB] relative py-3">
       <div className="w-[100%]">
         <div className="flex justify-between">
-          <h1 className="text-xl hover:cursor-pointer font-medium">
-            Dayal Tech
-          </h1>
-          <div className="hidden">
-            <ul className="space-y-2 flex space-x-5 md:space-y-0">
-              <li>Home</li>
-              <li>About</li>
-              <li>Services</li>
-              <li>Contact</li>
-              <li>Services</li>
-              <li>Login</li>
-            </ul>
-          </div>
+          <Link to="/">
+            <h1 className="text-xl hover:cursor-pointer font-medium">
+              Dayal Tech
+            </h1>
+          </Link>
           <div onClick={handleNavbar} className="md:hidden">
             <MenuIcon />
+          </div>
+          <div className="hidden md:block">
+            <ul className="space-y-2 flex space-x-7 md:space-y-0">
+              <Link to="/">
+                <li>Home</li>
+              </Link>
+              <Link to="/about">
+                <li>About</li>
+              </Link>
+              <Link to="/services">
+                <li>Services</li>
+              </Link>
+              <Link to="/contact">
+                <li>Contact</li>
+              </Link>
+              <Link to="/register">
+                <li>Register</li>
+              </Link>
+              <Link to="/login">
+                <li>Login</li>
+              </Link>
+            </ul>
           </div>
         </div>
 
