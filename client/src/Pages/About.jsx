d4b71ff59f0ec06dcc7daddca2354854e81ed873
Dayal Sudhakar <Dayalpython@gmail.com>
2024-02-13 19:01:46 +0530

    contact form in mobile version ready

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div>
+        <h1 className="text-5xl">Why Choose Us?</h1>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="h">
+      <h1 className="text-3xl font-bold mt-8">Contact us</h1>
+      <div>
+        <div>
+          <img
+            src="https://thapatechnical.site/images/support.png"
+            alt="contact us"
+          />
+        </div>
+
+        <form className="space-y-3 py-10">
+          <div className="flex flex-col">
+            <label className="pb-3">Name</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className="flex flex-col">
+            <label className="pb-3" htmlFor="email">
+              Email
+            </label>
+            <input type="email" />
+          </div>
+          <div className="flex flex-col">
+            <label className="pb-3" htmlFor="message">
+              Message
+            </label>
+            <textarea rows="8" cols="50" type="fieldtext" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
